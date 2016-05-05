99a8cfdc8e301766732ac00a2451623230f7c7db
Bing Steup <bingsteup@gmail.com>
2016-05-05 20:57:29 +0200

    test

--- index.php
+++ index.php
@@ -18,5 +18,9 @@ $post_password = trim($_POST['password']);
 #crypt password
 $post_password_salt = crypt($post_password,$salt);
 
-
+#create user and validateLogin
+$UserMasterObject = new UserClass();
+$json_out['login'] = $UserMasterObject->validateLoginJson($post_username,$post_password_salt);
+$post_username = strtolower(trim($_POST['username']));
+$post_password = trim($_POST['password']);
 ?>
