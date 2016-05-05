7f3d2d018836200abfc38986753683d6670a0df2
Bing Steup <bingsteup@gmail.com>
2016-05-05 20:53:40 +0200

    test

--- index2.php
+++ index2.php
@@ -1,38 +1,4 @@
 <?php
-
-error_reporting(E_ALL);
-
-#start session
-session_start();
-
-#require required files
-require '../constants.php';
-require '../dbinfo.inc.php';
-require '../tools.php';
-require '../user.php';
-
-#get posted username & password
-$post_username = strtolower(trim($_POST['username']));
-$post_password = trim($_POST['password']);
-
-#crypt password
-$post_password_salt = crypt($post_password,$salt);
-
-#create user and validateLogin
-$UserMasterObject = new UserClass();
-$json_out['login'] = $UserMasterObject->validateLoginJson($post_username,$post_password_salt);
-
-#if all json_out == NULL -> succes!
-if(!array_filter($json_out)){
-	$_SESSION['UserMasterObject'] = serialize($UserMasterObject);
-	$json_out = array('succes'=>'Logged in.');
-}
-
-#output json
-print json_encode($json_out);
-?>
-
-<?php
 /**************************************************
 
  _____     _            _          _____ _
