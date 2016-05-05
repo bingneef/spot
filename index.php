1198a7865b1b44cbb7cf8edc19fb8fd0477a4b3c
Bing Steup <bingsteup@gmail.com>
2016-05-05 20:55:11 +0200

    test

--- index.php
+++ index.php
@@ -4,10 +4,15 @@ error_reporting(E_ALL);
 
 #start session
 session_start();
+echo("here");
 
 #require required files
 require 'php/constants.php';
+echo("here");
 require 'php/bbinfo.inc.php';
+echo("here");
 require 'php/tools.php';
+echo("here");
 require 'php/user.php';
+echo("here");
 ?>
