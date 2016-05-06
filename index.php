bc19bfaa013634a057e65806ead904150051092e
Bing Steup <bingsteup@gmail.com>
2016-05-06 17:48:07 +0200

    change ga

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ if(!isset($UserMasterObject) || !$UserMasterObject->loggedIn()){
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-77301580-1', 'auto');
+  ga('create', 'UA-77301580-2', 'auto');
   ga('send', 'pageview');
 
 </script>
